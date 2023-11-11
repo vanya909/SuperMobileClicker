a799d6e1a1833ca50bb2ab5470afc05d91abeaf5
Marina <qumariet@gmail.com>
2023-11-11 21:09:04 +0700

    Part two of task 5 is done

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
 {
+    [SerializeField, Min(0)]
     private float destroyDelay;
+
+    [SerializeField, Min(0)]
     private int minimalDestroyingObjectsCount;
 
     private Transform myTransform;
@@ -14,6 +17,7 @@ public class DestroyModule : MonoBehaviour
         myTransform = transform;
     }
 
+    [ContextMenu("Destroy")]
     public void ActivateModule()
     {
         StartCoroutine(DestroyRandomChildObjectCoroutine());
---
+++ Assets/LearnMaterials 2/Scripts/MoveScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Profiling;
+using UnityEngine;
+
+public class MoveScript : SampleScript
+{
+    [SerializeField]
+    private Vector3 _destination;
+
+    [SerializeField, Min(0)]
+    private float _speed;
+
+    [ContextMenu("Move")]
+    public override void Use()
+    {
+        StartCoroutine(MoveToTarget());
+    }
+
+    private IEnumerator MoveToTarget()
+    {
+        float startTime = Time.time;
+        float journeyLength = Vector3.Distance(transform.position, _destination);
+
+        while (Vector3.Distance(transform.position, _destination) > 0)
+        {
+            float distCovered = (Time.time - startTime) * _speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(transform.position, _destination, fracJourney);
+            yield return null;
+        }
+    }
+}
